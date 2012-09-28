996d56684961450067b03e1915034838efbfc997
Sushil Kumar <0120sushil@gmail.com>
2012-09-28 22:44:44 +0530

    Increaded buffer size for better performance

--- src/me/uni/sushilkumar/ytgrabber/YTDownload.java
+++ src/me/uni/sushilkumar/ytgrabber/YTDownload.java
@@ -51,12 +51,10 @@ public class YTDownload extends HttpServlet {
         try {
 
             URL dUrl=new URL(url);
-            //URL dUrl=new URL("http://o-o---preferred---bharti-del2---v17---lscache7.c.youtube.com/videoplayback?upn=0SERusIEqXQ&sparams=cp%2Cgcr%2Cid%2Cip%2Cipbits%2Citag%2Cratebypass%2Csource%2Cupn%2Cexpire&fexp=907217%2C922401%2C919804%2C920704%2C912806%2C906831%2C911406%2C913550%2C912706&key=yt1&itag=37&ipbits=8&signature=C28F021E5E54DC2B50E84BFB2154F4FD817C6574.85903B32FBA5199D297CF91377B32640836D50A6&mv=m&sver=3&mt=1345687871&ratebypass=yes&source=youtube&ms=au&gcr=in&expire=1345711767&ip=116.203.237.173&cp=U0hTSldMT19LUUNOM19PRlNIOkJKQ1pDWjNzUGE5&id=9d8c9310d90eae67&quality=hd1080&fallback_host=tc.v17.cache7.c.youtube.com&type=video/mp4");
-            //URL dUrl=new URL("http://docs.oracle.com/cd/B19306_01/server.102/b14220.pdf");
             con=dUrl.openConnection();
             response.setContentLength(con.getContentLength());
             in=con.getInputStream();
-            buffer = new byte[1024];
+            buffer = new byte[4*1024];
             while ((ByteRead = in.read(buffer)) != -1)
             {
                 
