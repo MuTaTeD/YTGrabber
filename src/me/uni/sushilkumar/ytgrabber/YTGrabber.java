6c2feab74ee44dbd56e565696074b9fcfb8638c0
0120sushil <0120sushil@gmail.com>
2012-12-27 20:31:04 +0530

    Some fixtures in fetching links

--- src/me/uni/sushilkumar/ytgrabber/YTGrabber.java
+++ src/me/uni/sushilkumar/ytgrabber/YTGrabber.java
@@ -4,28 +4,38 @@
  */
 package me.uni.sushilkumar.ytgrabber;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
+import java.net.URL;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 
 /**
  *
  * @author sushil
  */
 public class YTGrabber extends HttpServlet {
-    private String title=null;
 
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+    private String title = null;
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -35,177 +45,167 @@ public class YTGrabber extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("application/json");
         PrintWriter out = response.getWriter();
-        String url=request.getParameter("url");
+        String url = request.getParameter("url");
         try {
-            if(url!=null)
-            {
-                if(url.indexOf("watch")<0)
-                {
-                    String json=generateMalformedUrlException();
-                    out.println(json);
-                }
-                
-                else
-                {
-                url=filterUrl(url);
-                String markup=getMarkup(url);
-                
-                String json=extractLinks(markup);
-                out.println(json);
-                }
+            int index = url.indexOf("&");
+            if (index >= 0) {
+                url = url.substring(0, index);
             }
-            else
-            {
-                String json=generateNoUrlException();
-                out.println(json);
-            }
-        } finally {            
+            index = url.indexOf("=");
+            String vid = url.substring(index + 1);
+            String json = extractLinks(vid);
+            out.println(json);
+        } finally {
             out.close();
         }
     }
-    
-    public String filterUrl(String url)
-    {
-        int index=url.indexOf("&");
-        if(index>=0)
-        url=url.substring(0,index);
-        return url;
-    }
-    
-    public String getMarkup(String url) throws IOException
-    {
-        int index1=0,index2=0;
-        Document doc=Jsoup.connect(url).get(); 
-        title=doc.title();
-        title= title.replaceAll("[^a-zA-Z0-9]+","");
-        String data=doc.outerHtml();
-        index1=data.indexOf("var swf =");
-        index2=data.indexOf("document.getElementById('watch-player').innerHTML = swf");
-        data=data.substring(index1+8,index2);
-        index1=data.indexOf("url_encoded_fmt_stream_map=");
-        data=data.substring(index1+27);
-        index2=data.indexOf("u0026amp;");
-	data=data.substring(0,index2-1);
-        data=URLDecoder.decode(data,"UTF-8");
-	
-        return data;
-    }
-    
-    public String filterLink(String link)
-    {
-        int index=0;
-		if(link.indexOf("codecs")>=0)
-		{
-			index=link.indexOf("codecs");
-			String temp1=link.substring(0,index-2);
-			index=link.indexOf("fallback");
-			String temp2=link.substring(index-1);
-			link=temp1+temp2;
-		}
-		
 
-		return link;
+    public Map<String, String> getQueryMap(String query) {
+        String[] params = query.split("&");
+        Map<String, String> map = new HashMap<String, String>();
+        for (String param : params) {
+            String[] values = param.split("=");
+
+            if (values.length > 1) {
+                map.put(values[0], values[1]);
+            } else {
+                map.put(values[0], "NA");
+            }
+
+
+        }
+        return map;
     }
-    
-    public String extractQuality(String link)
-    {
-        if(link.indexOf("small")>=0)
-            return "small(240p)";
-        else if(link.indexOf("medium")>=0)
-            return "medium(360p)";
-        else if(link.indexOf("large")>=0)
-            return "large(480p)";
-        else if(link.indexOf("hd720")>=0)
-            return "HD(720p)";
-        else if(link.indexOf("hd1080")>=0)
-            return "HD(1080p)";
-        return "Average Quality";
-    }
-    
-    public String extractFormat(String link)
-    {
-        if(link.indexOf("mp4")>0)
-            return "mp4";
-        else if(link.indexOf("flv")>0)
+
+    public String getExtension(String type) {
+        if (type.toLowerCase().equals("video/webm")) {
+            return "webm";
+        }
+        if (type.toLowerCase().equals("video/x-flv")) {
             return "flv";
-        else if(link.indexOf("3gpp")>0)
+        }
+        if (type.toLowerCase().equals("video/mp4")) {
+            return "mp4";
+        }
+        if (type.toLowerCase().equals("video/3gpp")) {
             return "3gpp";
-        return "Format";
+        }
+        return "format";
     }
-    
-    public String extractLinks(String data) throws UnsupportedEncodingException
-    {
-        JSONObject obj=new JSONObject();
-        JSONArray array=new JSONArray();
-        String current="",left=data;
-        while(left.indexOf("url")>=0)
-        {
-            JSONObject tempObj=new JSONObject();
-            int index1=left.indexOf("url");
-            int index2=left.indexOf(",");
-            if(index2>0)
-            {
-                current=left.substring(index1+4,index2);
-                current=URLDecoder.decode(current,"UTF-8");
-                left=left.substring(index2+1);
+
+    public String getQuality(String quality) {
+        if (quality.toLowerCase().equals("small")) {
+            return "240p";
+        }
+        if (quality.toLowerCase().equals("medium")) {
+            return "360p";
+        }
+        if (quality.toLowerCase().equals("large")) {
+            return "480p";
+        }
+        if (quality.toLowerCase().equals("hd720")) {
+            return "720p";
+        }
+        if (quality.toLowerCase().equals("hd1080")) {
+            return "1080p";
+        }
+        return "Normal";
+    }
+
+    public String extractLinks(String vid) {
+        InputStream inStream = null;
+        JSONObject obj = new JSONObject();
+        JSONArray array = new JSONArray();
+        try {
+            URL url = new URL("http://www.youtube.com/get_video_info?video_id=" + vid);
+            inStream = url.openStream();
+            BufferedReader in = new BufferedReader(new InputStreamReader(inStream));
+            String response = "", temp;
+            while ((temp = in.readLine()) != null) {
+                response += temp;
             }
-            else
-            {
-                current=left.substring(index1+4);
-                current=URLDecoder.decode(current,"UTF-8");
-                left="";
-            }
-            
-            if(current.indexOf("webm")<0)
-            {
-                current=filterLink(current);
-                current=current.replace('&','!');
-                current=replace(current,"sig", "signature");
-                tempObj.put("type",extractFormat(current)+" "+extractQuality(current));
-                tempObj.put("link","http://projects-sushilkumar.rhcloud.com/YTDownload?url="+current+"&title="+title+"."+extractFormat(current));
+            in.close();
+            Map<String, String> parameters = getQueryMap(response);
+            title = parameters.get("title");
+            title = URLDecoder.decode(title, "UTF-8");
+            title = title.replaceAll("[^a-zA-Z0-9\\s]+", "");
+            System.out.println(title);
+            String url_encoded_fmt_stream_map = parameters.get("url_encoded_fmt_stream_map");
+            url_encoded_fmt_stream_map = URLDecoder.decode(url_encoded_fmt_stream_map, "UTF-8");
+            String[] urls = url_encoded_fmt_stream_map.split(",");
+            for (String u : urls) {
+                Map<String, String> urlParameters = getQueryMap(u);
+
+
+                String type = urlParameters.get("type");
+                String quality = urlParameters.get("quality");
+                type = URLDecoder.decode(type, "UTF-8");
+                quality = URLDecoder.decode(quality, "UTF-8");
+                int index = type.indexOf(";");
+                if (index >= 0) {
+                    type = type.substring(0, index);
+                }
+                //System.out.println(type+"---->"+quality);
+                String fileName = title + "." + getExtension(type);
+                String ul = urlParameters.get("url") + "&signature=" + urlParameters.get("sig");
+                //System.out.println(title + "." + getExtension(type));
+                ul = URLDecoder.decode(ul, "UTF-8");
+                //ul=ul+ "&title=" +URLEncoder.encode(fileName,"UTF-8");
+                JSONObject tempObj = new JSONObject();
+                ul = ul.replace("&", "!");
+                ul = "http://projects-sushilkumar.rhcloud.com/YTDownload?url=" + ul + "&title=" + URLEncoder.encode(fileName, "UTF-8");
+                tempObj.put("link", ul);
+                tempObj.put("type", getExtension(type) + "/" + getQuality(quality));
                 array.add(tempObj);
+
+            }
+            obj.put("links", array);
+        } catch (IOException ex) {
+            Logger.getLogger(YTGrabber.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                inStream.close();
+            } catch (IOException ex) {
+                Logger.getLogger(YTGrabber.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        obj.put("links",array);
-        return obj.toString();
-        
-        
-    }
-    
-    public String generateNoUrlException()
-    {
-        JSONObject obj=new JSONObject();
-        obj.put("error_no","100");
-        obj.put("error","No URL Specified");
-        return obj.toJSONString();
-        
-    }
-    
-    public String generateMalformedUrlException()
-    {
-         JSONObject obj=new JSONObject();
-        obj.put("error_no","200");
-        obj.put("error","Not a valid URL");
         return obj.toJSONString();
     }
-    
-    static String replace(String str, String pattern, String replace) {
-    int s = 0;
-    int e = 0;
-    StringBuilder result = new StringBuilder();
 
-    while ((e = str.indexOf(pattern, s)) >= 0) {
-        result.append(str.substring(s, e));
-        result.append(replace);
-        s = e+pattern.length();
+    public String generateNoUrlException() {
+        JSONObject obj = new JSONObject();
+        obj.put("error_no", "100");
+        obj.put("error", "No URL Specified");
+        return obj.toJSONString();
+
     }
-    result.append(str.substring(s));
-    return result.toString();
-}
+
+    public String generateMalformedUrlException() {
+        JSONObject obj = new JSONObject();
+        obj.put("error_no", "200");
+        obj.put("error", "Not a valid URL");
+        return obj.toJSONString();
+    }
+
+    static String replace(String str, String pattern, String replace) {
+        int s = 0;
+        int e = 0;
+        StringBuilder result = new StringBuilder();
+
+        while ((e = str.indexOf(pattern, s)) >= 0) {
+            result.append(str.substring(s, e));
+            result.append(replace);
+            s = e + pattern.length();
+        }
+        result.append(str.substring(s));
+        return result.toString();
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
-     * Handles the HTTP <code>GET</code> method.
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -217,8 +217,10 @@ public class YTGrabber extends HttpServlet {
         processRequest(request, response);
     }
 
-    /** 
-     * Handles the HTTP <code>POST</code> method.
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -230,8 +232,9 @@ public class YTGrabber extends HttpServlet {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
